3990885fd4c94325c21ab3b7e4c80c7bed198108
virangh <virangh.vijayakumaran@hotmail.com>
2026-09-11 14:24:32 +0200

    Ajout de la passerelle Internet et du routage public

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -29,3 +29,29 @@ resource "aws_subnet" "private" {
     Name = "${var.name_prefix}-prive-a"
   }
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name_prefix}-rt-public"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
