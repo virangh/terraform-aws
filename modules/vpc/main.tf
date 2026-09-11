54da1b4178e9bfd283bfe1016d317f2b357e577a
virangh <virangh.vijayakumaran@hotmail.com>
2026-09-11 14:34:00 +0200

    Ajout de la NAT, de l'IP elastique et du routage prive

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,3 +55,41 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.name_prefix}-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id     = aws_eip.nat.id
+  subnet_id         = aws_subnet.public.id
+  connectivity_type = "public"
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.name_prefix}-nat"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name_prefix}-rt-prive"
+  }
+}
+
+resource "aws_route" "private_internet" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
